177a218de772be6297250fba412d6ea9eefcb8da
Sergio Bugallo <128857170+sejabu@users.noreply.github.com>
2025-02-07 10:50:28 -0300

    drafts

---
+++ Contracts/DigitalWillBasicFunctions.sol
@@ -0,0 +1,69 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+//Imports
+
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+
+contract DigitalWill is ReentrancyGuard, Pausable {
+
+//Variables
+
+//Eventos
+
+//Constructor
+
+//Funciones
+
+
+function createWill () external payable {
+
+}
+
+function listAssets () external {
+
+}
+
+function includeAsset () external {
+
+}
+
+function removeAsset () external {
+
+}
+
+function configLifeProof () external {
+
+}
+
+function setLifeProof () external {
+
+}
+
+function addBeneficiary () external {
+
+}
+
+function removeBeneficiary () external {
+
+}
+
+function setPercentages () external {
+
+}
+
+function acceptBeneficiary () external {
+
+}
+
+function initClaim () external {
+
+}
+
+function executeWill () external {
+
+}
+
+
+}
