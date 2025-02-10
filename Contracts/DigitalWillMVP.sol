8426a17a46044ac9ee298ef99cdfac058706b2a1
Sergio Bugallo <128857170+sejabu@users.noreply.github.com>
2025-02-10 08:44:45 -0300

    Update

--- Contracts/DigitalWillMVP.sol
+++ Contracts/DigitalWillMVP.sol
@@ -10,8 +10,8 @@ pragma solidity ^0.8.26;
 
 //LIBRERIAS:
 
-import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 // import "@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
