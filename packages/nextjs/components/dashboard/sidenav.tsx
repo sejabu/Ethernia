53f5bdf30c3e71cb937dea74bae1ae73bbbbc1d2
Sergio Bugallo <128857170+sejabu@users.noreply.github.com>
2025-03-17 08:44:54 -0300

    update

--- packages/nextjs/components/dashboard/sidenav.tsx
+++
@@ -1,9 +0,0 @@
-import NavLinks from './nav-links';
-
-export default function SideNav() {
-  return (
-      <div className="flex grow flex-row justify-between space-x-2 w-full h-full p-2">
-        <NavLinks />
-      </div>
-  );
-}
